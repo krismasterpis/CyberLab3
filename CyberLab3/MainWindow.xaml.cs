b05166e3218e96915dc91acb62285c1b064d0d56
DELL <krismaster0808@gmail.com>
2025-07-10 12:15:00 +0200

    Dodaj pliki projektów.

---
+++ CyberLab3/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using CyberLab3.Resources.Controls;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CyberLab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            sideBar.SelectedIndex = 0;
+        }
+
+        private void sideBar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = sideBar.SelectedItem as NavButton;
+            if(selected != null && navFrame != null)
+            {
+                navFrame.Navigate(selected.NavLink);
+                navInfoBar.Text = selected.NavName;
+            }
+        }
+    }
+}
---
+++ CyberLab3/Resources/Controls/NavInfoBar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CyberLab3.Resources.Controls
+{
+    public class NavInfoBar : Control
+    {
+        static NavInfoBar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NavInfoBar), new FrameworkPropertyMetadata(typeof(NavInfoBar)));
+        }
+
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(NavInfoBar), new PropertyMetadata(string.Empty));
+
+
+    }
+}
